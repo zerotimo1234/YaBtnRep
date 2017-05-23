92fb028820b23418944238a36eea66732a2bbdfe
_haha_ <zerogo8080@163.com>
2017-05-23 18:25:18 +0800

    1xxx

---
+++ Classes/YaBtn.h
@@ -0,0 +1,13 @@
+//
+//  YaBtn.h
+//  DayTestDemo
+//
+//  Created by Yazi on 2017/5/23.
+//  Copyright © 2017年 Yazi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface YaBtn : UIButton
+
+@end
---
+++ Classes/YaBtn.m
@@ -0,0 +1,21 @@
+//
+//  YaBtn.m
+//  DayTestDemo
+//
+//  Created by Yazi on 2017/5/23.
+//  Copyright © 2017年 Yazi. All rights reserved.
+//
+
+#import "YaBtn.h"
+
+@implementation YaBtn
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
